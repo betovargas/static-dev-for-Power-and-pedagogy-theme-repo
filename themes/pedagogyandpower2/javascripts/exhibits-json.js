819a7a5f193a449bd89398e3b365e962379e4d62
betovargas <rvargas1@swarthmore.edu>
2019-01-09 10:59:30 -0500

    edits everything

---
+++ themes/pedagogyandpower2/javascripts/exhibits-json.js
@@ -0,0 +1,9 @@
+var exhibitsJSON =
+[
+  {
+    "title": "Race & Ed",
+    "description": "A curated collection of resources that examine race and education in the United States.",
+    "url": "/exhibits/show/race.html",
+    "thumb": "square_thumbnails/08749f3f7257ec5380f510fa7fd45d7b.jpg"
+  }
+]
---
+++ themes/pedagogyandpower2/javascripts/main-carousel.js
@@ -0,0 +1,32 @@
+// main page carousel
+
+ 
+jQuery(function(){
+
+   var s3Prefix = 'http://s3.amazonaws.com/sc-lib-ds-pedagogyandpower/';
+   var carousel = jQuery('.jcarousel#jcarousel-main ul');
+
+    for(i in exhibitsJSON){
+
+        var exhibit = "<li><a href='" + exhibitsJSON[i].url + "'>";
+        exhibit += "<div style='background: url(";
+        exhibit += s3Prefix;
+        exhibit += exhibitsJSON[i].thumb + ")'>";
+        exhibit += "<div><h2>" + exhibitsJSON[i].title + "</h2>";
+        exhibit += "<p>" + exhibitsJSON[i].description + "</p></div></div></li>"
+        
+        carousel.append(exhibit);
+    }
+
+    jQuery('.jcarousel#jcarousel-main')
+        .on('jcarousel:create jcarousel:reload', function(){
+
+            var el = jQuery(this),
+                width = el.innerWidth();
+                el.jcarousel('items').css('width',width + 'px');
+        })
+        .jcarousel({
+            animation: 'slow'
+        })
+        .jcarousel('reload');
+});
